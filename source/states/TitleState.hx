e7447a60635295b7f73f2ead45e8a04fc06fcd83
Sunny <gnovatosarchive@gmail.com>
2026-03-22 10:40:53 -0300

    Update music BPM and change text in TitleState

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -220,7 +220,7 @@ class TitleState extends MusicBeatState
 	var enterPosition:FlxPoint = FlxPoint.get(100, 576);
 	
 	var useIdle:Bool = false;
-	var musicBPM:Float = 102;
+	var musicBPM:Float = 136;
 	var danceLeftFrames:Array<Int> = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
 	var danceRightFrames:Array<Int> = [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
@@ -548,11 +548,11 @@ class TitleState extends MusicBeatState
 				case 13:
 					deleteCoolText();
 				case 14:
-					addMoreText('Friday');
+					addMoreText('April');
 				case 15:
-					addMoreText('Night');
+					addMoreText('The');
 				case 16:
-					addMoreText('Funkin'); // credTextShit.text += '\nFunkin';
+					addMoreText('1st'); // credTextShit.text += '\nFunkin';
 
 				case 17:
 					skipIntro();
